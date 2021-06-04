19175ea12c9c853b64e1ace8e1dc076ef5a36f1e
nitheesh12-creator <85355123+nitheesh12-creator@users.noreply.github.com>
2021-06-05 00:42:55 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -5,33 +5,28 @@
 
 float wallis_pi(int);
 
-
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      abort();
+    pi = wallis_pi(i);if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);abort();
     }
   }
 
   for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
+    if (!(fabs(pi - M_PI) < 0.01)){
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
       abort();
     }
   }
-}
+}      
 float wallis_pi(int n)
-{
- float product;
- product=2.0;
- int i,j;
- for(i=1;i<n+1;i++)
- {
-  product=(product*4*i*i)/(4*i*i-1);
- }
- return product;
+  { float p=1;
+  for(int i=1;i<=n ;i++)
+  {float e=4*i*i;
+  float k=(float)e/(e-1);
+  p=p*k;
+  }return p*2;
+  
 }
