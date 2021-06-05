c43f1447ce20b5a906897d78bdc675d18280cee6
nitheesh12-creator <85355123+nitheesh12-creator@users.noreply.github.com>
2021-06-05 20:44:54 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -13,8 +13,7 @@ int main(void) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
-  }
-for (int i=500; i<3000; i++) {
+   for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) < 0.01)) {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
@@ -28,5 +27,5 @@ float wallis_pi(int n)
   {float e=4*i*i;
   float k=(float)e/(e-1);
   p=p*k;
-  }return p*2;
-}
+  }return p*2
+   }
