c0eeba4332f2a2b549683260edcc1c20aee76024
nitheesh12-creator <85355123+nitheesh12-creator@users.noreply.github.com>
2021-06-05 21:01:30 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 float wallis_pi(int);
+
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
@@ -10,9 +12,9 @@ int main(void) {
     if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
-      }
-  }
-}
+    }
+  }[1:55 PM, 6/4/2021] Pinnem njn thanneey: 
+
   for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) < 0.01)) {
@@ -27,5 +29,41 @@ float wallis_pi(int n)
   {float e=4*i*i;
   float k=(float)e/(e-1);
   p=p*k;
-  }return p*2;  
+  }return p*2;
+  
+}
+[8:42 PM, 6/5/2021] Pinnem njn thanneey: nitheesh@24
+[8:42 PM, 6/5/2021] Pinnem njn thanneey: nitheesh12-creator
+[8:54 PM, 6/5/2021] Pinnem njn thanneey: #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+float wallis_pi(int);
+
+int main(void) {
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
+
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
+}
+float wallis_pi(int n)
+  { float p=1;
+  for(int i=1;i<=n ;i++)
+  {float e=4*i*i;
+  float k=(float)e/(e-1);
+  p=p*k;
+  }return p*2;
   }
